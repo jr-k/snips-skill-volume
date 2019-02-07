7aa700b7b096e3196a58d6cc95fd401653dfec89
root <root@arx2>
2019-02-07 02:04:00 +0000

    first

---
+++ action-volume.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+from snipsTools import SnipsConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+import paho.mqtt.client as paho
+import json
+import random
+
+CONFIG_INI = "config.ini"
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+ACK = ["Trait bien", "OK", "daccord", "sait fait", "Pas de problaime", "entendu"]
+
+class Volume(object):
+    """Class used to wrap action code with mqtt connection
+
+        Please change the name refering to your application
+    """
+    def __init__(self):
+        # get the configuration if needed
+        try:
+            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+        except :
+            self.config = None
+
+
+        # start listening to MQTT
+        self.start_blocking()
+
+    # --> Sub callback function, one per intent
+    def setVolumeCallback(self, hermes, intent_message):
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        # action code goes here...
+        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+
+        volume = 5
+
+        if intent_message.slots.volume:
+            volume = intent_message.slots.volume.first().value
+
+        print "Volume: " + str(volume)
+
+        tmpClient = paho.Client("tmp_hub")  # create client object
+        tmpClient.connect(MQTT_IP_ADDR, MQTT_PORT)  # establish connection
+        tmpClient.loop_start()
+        tmpClient.publish("sat/volume", json.dumps({"volume": volume, "siteId": intent_message.site_id}))
+        tmpClient.loop_stop()
+        tmpClient.disconnect()
+
+        # if need to speak the execution result by tts
+        hermes.publish_start_session_notification(intent_message.site_id, ACK[random.randint(0,len(ACK) - 1)], "")
+
+    # --> Master callback function, triggered everytime an intent is recognized
+    def master_intent_callback(self,hermes, intent_message):
+
+        intent_name = intent_message.intent.intent_name
+        if ':' in intent_name:
+            intent_name = intent_name.split(":")[1]
+        if intent_name == 'setVolume':
+            self.setVolumeCallback(hermes, intent_message)
+
+    # --> Register callback function and start MQTT
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subscribe_intents(self.master_intent_callback).start()
+
+if __name__ == "__main__":
+    Volume()
